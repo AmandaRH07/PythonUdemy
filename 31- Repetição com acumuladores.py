571b5056a1777f095d49ad7b29e7737c43853fad
AmandaRH07 <amandarafahass13@gmail.com>
2020-10-09 11:13:59 -0300

    Apagando arquivos duplicados e fora da pasta

--- 31- Repetição com acumuladores.py
+++
@@ -1,9 +0,0 @@
-cont = 1
-acumulador =1
-
-while cont <= 10:
-    print(cont, acumulador)
-    acumulador += cont
-    cont += 1
-else:
-    print("Else.")
--- 36- Desempacotamento de Listas.py
+++
@@ -1,5 +0,0 @@
-lista = ["Amanda", "Maria", "João",1,2,3,4,5,6,100]
-
-n1, n2, *outra_lista, ultimo_da_lista = lista
-
-print(n1, n2,outra_lista, ultimo_da_lista)
